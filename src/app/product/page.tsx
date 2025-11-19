aefffd1ce339d0921f353d5f0480eeaaa983c082
fatimarezaie256 <fatimarezaie112@gmail.com>
2025-11-19 10:17:10 +0430

    one more page added

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,13 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
-
+const list:{id:number,name:string,price:number}[]=[
+  {id:1,name:"Apple",price:140},
+  {id:2,name:"Banana",price:100},
+  {id:3,name:"Peach",price:90},
+  {id:4,name:"Kewi",price:290},
+  {id:5,name:"Cherrri",price:120},
+  {id:6,name:"Orange",price:240},
+]
 function page() {
   return (
-    <div>
-        <h1 style={{fontSize:"30px",fontFamily:"cursive", textAlign:"center",backgroundColor:"gray",color:"white",padding:"8px 6px"}}>The product page</h1>
-        <h1 style={{fontSize:"25px",}}>product 1</h1>
-        <h1 style={{fontSize:"25px",}}>product 2</h1>
-        <h1 style={{fontSize:"25px",}}>product 3</h1>
-        <h1 style={{fontSize:"25px",}}>product 4</h1>
+    <div style={{display:"grid",gridRow:"3",width:"100%"}}>
+        {list.map(product=>(
+           <div style={{height:"100%",width:"30%",textAlign:"center",backgroundColor:"black",padding:"4px",borderRadius:"30px",color:"white"}}>
+            <h1 style={{}}>{product.id}</h1>
+            <h1 style={{fontSize:"50px"}}>{product.name}</h1>
+            <h1 style={{}}>{product.price}</h1>
+            <Link style={{padding:"9px 16px",backgroundColor:"blue",color:"white",borderRadius:"10px"}} href={`products/order/${product.id}`}>Order</Link>
+           </div>
+        ))}
     </div>
   )
 }
